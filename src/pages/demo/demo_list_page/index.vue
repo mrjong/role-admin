368a7cac6f1d12b79828034e88b8a46dd96184e8
shawn2016 <787990963@qq.com>
2019-01-21 21:37:18 +0800

    --

--- src/pages/demo/demo_list_page/index.vue
+++ src/pages/demo/demo_list_page/index.vue
@@ -1,6 +1,12 @@
 <template>
 
   <div class="panel_list">
+    <Modal
+      width="90%"
+      v-model="visible1"
+    >
+      <icon-list></icon-list>
+    </Modal>
     <!-- 检索条件 -->
     <Card class="vue-panel">
       <p
@@ -9,13 +15,12 @@
       >
         <Icon :type="!showPanel?'chevron-down':'chevron-up'"></Icon>
         检索条件
-        <router-link to="/demo/demo_desc">
           <Button
+          @click.stop="openIcon"
             class="fr vue-back-btn header-btn"
             type="primary"
             size="small"
-          >详情</Button>
-        </router-link>
+          >icon选择</Button>
       </p>
       <Form
         v-if="!showPanel"
