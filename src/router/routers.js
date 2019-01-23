9cde35ef9b2fbeaa2c19555e9d30560c2c6f6af0
shawn2016 <787990963@qq.com>
2019-01-23 17:04:51 +0800

    路由

--- src/router/routers.js
+++ src/router/routers.js
@@ -1,7 +1,9 @@
 import CommonRouter from '@/pages/common/router';
 import DemoRouter from '@/pages/demo/router';
+import collectionMngRouter from '@/pages/collectionMng/router';
 
 export default {
 	...CommonRouter,
-	...DemoRouter
+	...DemoRouter,
+	...collectionMngRouter
 };
