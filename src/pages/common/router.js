561d8de4f309c98790d05bfd48bb7cdf0384b904
shawn2016 <787990963@qq.com>
2019-01-23 17:14:41 +0800

    --

--- src/pages/common/router.js
+++
@@ -1,6 +0,0 @@
-export default {
-	'/common/home': () => import('@/pages/common/home'),
-	'/common/message': () => import('@/pages/common/message'),
-	'/common/main': () => import('@/pages/common/main'),
-	'/common/error-page': () => import('@/pages/common/error-page')
-};
--- src/router/routers.js
+++ src/router/routers.js
@@ -1,9 +1,10 @@
-import CommonRouter from '@/pages/common/router';
 import DemoRouter from '@/pages/demo/router';
 import collectionMngRouter from '@/pages/collectionMng/router';
-
+import workflowRouter from '@/pages/workflow/router';
+import arbitramentMngRouter from '@/pages/arbitramentMng/router';
 export default {
-	...CommonRouter,
 	...DemoRouter,
-	...collectionMngRouter
+	...collectionMngRouter,
+	...workflowRouter,
+	...arbitramentMngRouter
 };
