bd409dff7aa0c266c9f36108374327c50c909616
shawn2016 <787990963@qq.com>
2019-02-21 17:09:07 +0800

    --

--- src/pages/system/menu_manager_page/index.js
+++ src/pages/system/menu_manager_page/index.js
@@ -1,269 +1,304 @@
 import { stytem_menu_list, stytem_menu_update, stytem_menu_add, system_menu_del } from '@/service/getData';
-import IconList from '@/components/iconList'
+import IconList from '@/components/iconList';
 export default {
-  components: {
-    IconList,
-  },
-  data() {
-    return {
-      showPanel: false,
-      showPanel2: false,
-      detailFlag: false,
-      showIconFlag: false,
-      iconType: '',
-      modal: '',
-      itemName: '',
-      data: {},
-      data5: [],
-      buttonProps: {
-        type: 'primary',
-        size: 'small',
-      },
-      ruleValidate1: {
-        text: [
-          {
-            required: true,
-            message: "请输入菜单名称",
-            trigger: "blur"
-          }
-        ],
-        sort: [
-          {
-            required: true,
-            message: "请输入位置",
-            trigger: "blur"
-          }
-        ],
-        url: [
-          {
-            required: true,
-            message: "请输入url",
-            trigger: "blur"
-          }
-        ],
-      },
-      ruleValidate2: {
-        text: [
-          {
-            required: true,
-            message: "请输入菜单名称",
-            trigger: "blur"
-          }
-        ],
-        sort: [
-          {
-            required: true,
-            message: "请输入位置",
-            trigger: "blur"
-          }
-        ],
-        url: [
-          {
-            required: true,
-            message: "请输入url",
-            trigger: "blur"
-          }
-        ],
-      },
-      menuFormItem: {
-        parent: '',
-        text: '',
-        icon: '',
-        url: '',
-      },
-      menuAddFormItem: {
-        text: '',
-        icon: '',
-        url: '',
-        parent: '',
-      },
-    }
-  },
-  created() {
-    this.getList()
-  },
-  methods: {
-    // 选中节点的回调函数
-    selectNode(node) {
-      console.log(node)
-    },
-    renderContent(h, { root, node, data }) {
-      return h('span', {
-        style: {
-          display: 'inline-block',
-          width: '94%',
-          boxSizing: 'border-box',
-        }
-      }, [
-          h('span', [
-            h('Icon', {
-              props: {
-                type: data.icon,
-              },
-              style: {
-                marginRight: '4px'
-              }
-            }),
-            h('span', {
-              props: {
-              },
-              style: {
-                cursor: 'pointer'
-              },
-              class: 'tree_title',
-              on: {
-                'click': (e) => {
-                  let titleSpan = document.querySelectorAll('.tree_title');
-                  titleSpan.forEach(item => {
-                    item.className = 'tree_title';
-                  });
-                  if (e.target.className.indexOf('ivu-tree-title-selected') === -1) {
-                    e.target.className = 'tree_title ivu-tree-title-selected';
-                  };
-                  this.modal = '1';
-                  this.menuFormItem = {
-                    parent: data.parent,
-                    id: data.id,
-                    text: data.text,
-                    icon: data.icon,
-                    url: data.url,
-                    sort: String(data.sort),
-                  }
-                }
-              }
-            }, data.text)
-          ]),
-          h('span', {
-            style: {
-              display: 'inline-block',
-              float: 'right',
-              marginRight: '20px'
-            }
-          }, [
-              h('Button', {
-                props: Object.assign({}, this.buttonProps, {
-                  // icon: 'ios-plus-empty'
-                  type: 'primary'
-                }),
-                style: {
-                  // marginRight: data.type === '4' || data.type === '3' ? 0 : '4px'
-                },
-                on: {
-                  click: () => { this.addItem(data) }
-                }
-              }, '添加'),
-              h('Button', {
-                props: Object.assign({}, this.buttonProps, {
-                  // icon: 'ios-plus-empty'
-                  type: 'error'
-                }),
-                style: {
-                  marginLeft: '4px'
-                },
-                on: {
-                  click: () => {
-                    this.system_menu_del(data.id);
-                  }
-                }
-              }, '删除'),
-            ])
-        ]);
-    },
-    showIconList(type) {
-      this.iconType = type;
-      this.showIconFlag = true;
-    },
-    // iconlist子组件回调
-    passBack(icon) {
-      console.log(icon);
-      if (this.iconType === 0) {
-        this.menuAddFormItem.icon = icon;
-      } else {
-        this.menuFormItem.icon = icon;
-      };
-    },
-    selectIcon(type) {
-      if (type === 1) {
-        if (this.iconType === 0) {
-          this.menuAddFormItem.icon = '';
-        } else {
-          this.menuFormItem.icon = '';
-        };
-      };
-      this.showIconFlag = false;
-    },
-    // 提交保存修改
-    handleSubmit(name, type) {
-      this.$refs[name].validate(valid => {
-        if (valid) {
-          if (type === 1) {
-            this.menuUpdate(this.menuFormItem);
-          } else {
-            this.menuAddFormItem.parent = this.data.id;
-            this.menuAdd(this.menuAddFormItem);
-          }
-        } else {
-          this.$Message.error("查询条件格式有误，请重新填写");
-        }
-      });
-    },
+	components: {
+		IconList
+	},
+	data() {
+		return {
+			showPanel: false,
+			showPanel2: false,
+			detailFlag: false,
+			showIconFlag: false,
+			iconType: '',
+			modal: '',
+			itemName: '',
+			data: {},
+			data5: [],
+			buttonProps: {
+				type: 'primary',
+				size: 'small'
+			},
+			ruleValidate1: {
+				text: [
+					{
+						required: true,
+						message: '请输入菜单名称',
+						trigger: 'blur'
+					}
+				],
+				sort: [
+					{
+						required: true,
+						message: '请输入位置',
+						trigger: 'blur'
+					}
+				],
+				url: [
+					{
+						required: true,
+						message: '请输入url',
+						trigger: 'blur'
+					}
+				]
+			},
+			ruleValidate2: {
+				text: [
+					{
+						required: true,
+						message: '请输入菜单名称',
+						trigger: 'blur'
+					}
+				],
+				sort: [
+					{
+						required: true,
+						message: '请输入位置',
+						trigger: 'blur'
+					}
+				],
+				url: [
+					{
+						required: true,
+						message: '请输入url',
+						trigger: 'blur'
+					}
+				]
+			},
+			menuFormItem: {
+				parent: '',
+				text: '',
+				icon: '',
+				url: ''
+			},
+			menuAddFormItem: {
+				text: '',
+				icon: '',
+				url: '',
+				parent: ''
+			}
+		};
+	},
+	created() {
+		this.getList();
+	},
+	methods: {
+		// 选中节点的回调函数
+		selectNode(node) {
+			console.log(node);
+		},
+		renderContent(h, { root, node, data }) {
+			return h(
+				'span',
+				{
+					style: {
+						display: 'inline-block',
+						width: '94%',
+						boxSizing: 'border-box'
+					}
+				},
+				[
+					h('span', [
+						h('Icon', {
+							props: {
+								type: data.icon
+							},
+							style: {
+								marginRight: '4px'
+							}
+						}),
+						h(
+							'span',
+							{
+								props: {},
+								style: {
+									cursor: 'pointer'
+								},
+								class: 'tree_title',
+								on: {
+									click: (e) => {
+										let titleSpan = document.querySelectorAll('.tree_title');
+										titleSpan.forEach((item) => {
+											item.className = 'tree_title';
+										});
+										if (e.target.className.indexOf('ivu-tree-title-selected') === -1) {
+											e.target.className = 'tree_title ivu-tree-title-selected';
+										}
+										this.modal = '1';
+										this.menuFormItem = {
+											parent: data.parent,
+											id: data.id,
+											text: data.text,
+											icon: data.icon,
+											url: data.url,
+											sort: String(data.sort)
+										};
+									}
+								}
+							},
+							data.text
+						)
+					]),
+					h(
+						'span',
+						{
+							style: {
+								display: 'inline-block',
+								float: 'right',
+								marginRight: '20px'
+							}
+						},
+						[
+							h(
+								'Button',
+								{
+									props: Object.assign({}, this.buttonProps, {
+										// icon: 'ios-plus-empty'
+										type: 'primary'
+									}),
+									style: {
+										// marginRight: data.type === '4' || data.type === '3' ? 0 : '4px'
+									},
+									on: {
+										click: () => {
+											this.addItem(data);
+										}
+									}
+								},
+								'添加'
+							),
+							h(
+								'Poptip',
+								{
+									props: {
+										confirm: true,
+										title: '您确定要删除这条数据吗?',
+										transfer: true
+									},
+									on: {
+										'on-ok': () => {
+											this.system_menu_del(data.id);
+										}
+									}
+								},
+								[
+									h(
+										'Button',
+										{
+											props: Object.assign({}, this.buttonProps, {
+												// icon: 'ios-plus-empty'
+												type: 'error'
+											}),
+											style: {
+												marginLeft: '4px'
+											},
+											
+										},
+										'删除'
+									)
+								]
+							)
+						]
+					)
+				]
+			);
+		},
+		showIconList(type) {
+			this.iconType = type;
+			this.showIconFlag = true;
+		},
+		// iconlist子组件回调
+		passBack(icon) {
+			console.log(icon);
+			if (this.iconType === 0) {
+				this.menuAddFormItem.icon = icon;
+			} else {
+				this.menuFormItem.icon = icon;
+			}
+			this.showIconFlag = false;
+		},
+		selectIcon(type) {
+			if (type === 1) {
+				if (this.iconType === 0) {
+					this.menuAddFormItem.icon = '';
+				} else {
+					this.menuFormItem.icon = '';
+				}
+			}
+			this.showIconFlag = false;
+		},
+		// 提交保存修改
+		handleSubmit(name, type) {
+			this.$refs[name].validate((valid) => {
+				if (valid) {
+					if (type === 1) {
+						this.menuUpdate(this.menuFormItem);
+					} else {
+						this.menuAddFormItem.parent = this.data.id;
+						this.menuAdd(this.menuAddFormItem);
+					}
+				} else {
+					this.$Message.error('查询条件格式有误，请重新填写');
+				}
+			});
+		},
 
-    // 添加菜单项
-    addItem(data) {
-      this.menuAddFormItem = {
-        text: '',
-        icon: '',
-        url: '',
-        parent: '',
-      },
-      this.modal = '0';
-      this.data = data;
-    },
-    cancel() {
-      this.modal = '';
-    },
-    // 获取表格数据
-    async getList(params) {
-      const res = await stytem_menu_list({ state: '01' });
-      console.log(res)
-      if (res.code) {
-        this.data5 = res.data;
-        this.data5[0].expand = true;
-        console.log(this.data5);
-      } else {
-        this.$Message.error(res.message);
-      }
-    },
-    // 修改菜单项
-    async menuUpdate(params) {
-      const res = await stytem_menu_update(params);
-      if (res.code === 1) {
-        this.$Message.success('更新成功');
-        this.getList();
-      } else {
-        this.$Message.error(res.message);
-      }
-    },
-    // 新增菜单项
-    async menuAdd(params) {
-      const res = await stytem_menu_add(params);
-      if (res.code === 1) {
-        this.$Message.success('添加成功');
-        this.modal = '';
-        this.getList();
-      } else {
-        this.$Message.error(res.message);
-      }
-    },
-    // 删除菜单项
-    async system_menu_del(id) {
-      const res = await system_menu_del({id: id});
-      if (res.code === 1) {
-        this.$Message.success('删除成功');
-        this.getList();
-      } else {
-        this.$Message.error(res.message);
-      }
-    },
-  },
-}
+		// 添加菜单项
+		addItem(data) {
+			(this.menuAddFormItem = {
+				text: '',
+				icon: '',
+				url: '',
+				parent: ''
+			}),
+				(this.modal = '0');
+			this.data = data;
+		},
+		cancel() {
+			this.modal = '';
+		},
+		// 获取表格数据
+		async getList(params) {
+			const res = await stytem_menu_list({ state: '01' });
+			console.log(res);
+			if (res.code) {
+				this.data5 = res.data;
+				this.data5[0].expand = true;
+				console.log(this.data5);
+			} else {
+				this.$Message.error(res.message);
+			}
+		},
+		// 修改菜单项
+		async menuUpdate(params) {
+			const res = await stytem_menu_update(params);
+			if (res.code === 1) {
+				this.$Message.success('更新成功');
+				this.getList();
+			} else {
+				this.$Message.error(res.message);
+			}
+		},
+		// 新增菜单项
+		async menuAdd(params) {
+			const res = await stytem_menu_add(params);
+			if (res.code === 1) {
+				this.$Message.success('添加成功');
+				this.modal = '';
+				this.getList();
+			} else {
+				this.$Message.error(res.message);
+			}
+		},
+		// 删除菜单项
+		async system_menu_del(id) {
+			const res = await system_menu_del({ id: id });
+			if (res.code === 1) {
+				this.$Message.success('删除成功');
+				this.getList();
+			} else {
+				this.$Message.error(res.message);
+			}
+		}
+	}
+};
--- src/pages/system/menu_manager_page/index.vue
+++ src/pages/system/menu_manager_page/index.vue
@@ -5,7 +5,7 @@
       <Col span="8" class="tree-col">
         <Card class="vue-panel tree-card">
           <p slot="title">
-            <Icon :type="!showPanel?'chevron-down':'chevron-up'" @click="showPanel=!showPanel"></Icon>菜单
+            <Icon :type="!showPanel?'chevron-down':'chevron-up'" @click="showPanel=!showPanel"></Icon> 菜单
             <!-- <router-link to="/demo/demo_desc">
               <Button class="fr vue-back-btn header-btn" type="primary" size="small">详情</Button>
             </router-link>-->
@@ -33,27 +33,28 @@
           <p slot="title">修改信息</p>
           <!-- 菜单项详情 -->
           <Form ref="menuFormItem" :model="menuFormItem" :label-width="90" :rules="ruleValidate1">
-            <Col :xs="24" :sm="24" :md="10" :lg="10" span="4">
+            <Col :xs="24" :sm="24" :md="20" :lg="20" span="4">
               <FormItem span="4" label="菜单名称:" prop="text">
                 <Input size="small" clearable v-model="menuFormItem.text" placeholder="请输入菜单名称"></Input>
               </FormItem>
             </Col>
             <Col :xs="24" :sm="24" :md="20" :lg="20" span="4">
               <FormItem span="4" label="位置:" prop="sort">
-                <Input size="small" v-model="menuFormItem.sort" placeholder="请输入位置"></Input>
+                <Input size="small" v-model="menuFormItem.sort" clearable placeholder="请输入位置"></Input>
               </FormItem>
             </Col>
             <Col :xs="24" :sm="24" :md="20" :lg="20" span="4">
               <FormItem span="4" label="URL:" prop="url">
-                <Input size="small" v-model="menuFormItem.url" placeholder="请输入url"></Input>
+                <Input size="small" v-model="menuFormItem.url" clearable placeholder="请输入url"></Input>
               </FormItem>
             </Col>
             <Col :xs="24" :sm="24" :md="20" :lg="20" span="4">
               <FormItem span="4" label="图标:">
-                <Input size="small" v-model="menuFormItem.icon" disabled placeholder="请选择图标"></Input>
-                <Button type="primary" size="small" @click="showIconList(1)">选择图标</Button>
+                <Input size="small" v-model="menuFormItem.icon" disabled clearable placeholder="请选择图标"></Input>
+                <Button :icon="menuFormItem.icon" type="primary" size="small" @click="showIconList(1)">选择图标</Button>
               </FormItem>
             </Col>
+           
             <Col :xs="24" :sm="24" :md="24" :lg="24" span="6">
               <FormItem>
                 <Button type="ghost" size="small" @click="cancel">取消</Button>
@@ -74,25 +75,25 @@
             :label-width="90"
             :rules="ruleValidate2"
           >
-            <Col :xs="24" :sm="24" :md="10" :lg="10" span="4">
+            <Col :xs="24" :sm="24" :md="20" :lg="20" span="4">
               <FormItem span="4" label="菜单名称:" prop="text">
-                <Input size="small" clearable v-model="menuAddFormItem.text" placeholder="请输入菜单名称"></Input>
+                <Input size="small" clearable v-model="menuAddFormItem.text" clearable placeholder="请输入菜单名称"></Input>
               </FormItem>
             </Col>
             <Col :xs="24" :sm="24" :md="20" :lg="20" span="4">
               <FormItem span="4" label="位置:" prop="sort">
-                <Input size="small" v-model="menuAddFormItem.sort" placeholder="请输入位置"></Input>
+                <Input size="small" v-model="menuAddFormItem.sort" clearable placeholder="请输入位置"></Input>
               </FormItem>
             </Col>
             <Col :xs="24" :sm="24" :md="20" :lg="20" span="4">
               <FormItem span="4" label="URL:" prop="url">
-                <Input size="small" v-model="menuAddFormItem.url" placeholder="请输入url"></Input>
+                <Input size="small" v-model="menuAddFormItem.url" clearable placeholder="请输入url"></Input>
               </FormItem>
             </Col>
             <Col :xs="24" :sm="24" :md="20" :lg="20" span="4">
               <FormItem span="4" label="图标:">
-                <Input size="small" v-model="menuAddFormItem.icon" disabled placeholder="请选择图标"></Input>
-                <Button type="primary" size="small" @click="showIconList(0)">选择图标</Button>
+                <Input size="small" v-model="menuAddFormItem.icon" clearable disabled placeholder="请选择图标"></Input>
+                <Button  :icon="menuAddFormItem.icon" type="primary" size="small" @click="showIconList(0)">选择图标</Button>
               </FormItem>
             </Col>
             <Col :xs="24" :sm="24" :md="24" :lg="24" span="6">
