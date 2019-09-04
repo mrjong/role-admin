cd9c94fd776edf9c0e7f310830a57e87cd0ead58
sun_zhuo <>
2019-09-04 18:35:53 +0800

    优化项完成

--- src/pages/dailyMonitoring/router.js
+++ src/pages/dailyMonitoring/router.js
@@ -1,6 +1,5 @@
 export default {
   '/dailyMonitoring/overduePayment': () => import('@/pages/dailyMonitoring/overduePayment'),
-  '/dailyMonitoring/recovery': () => import('@/pages/dailyMonitoring/recovery'),
   '/dailyMonitoring/seatTable': () => import('@/pages/dailyMonitoring/seatTable'),
   '/dailyMonitoring/callDetail': () => import('@/pages/dailyMonitoring/callDetail'),
 }
