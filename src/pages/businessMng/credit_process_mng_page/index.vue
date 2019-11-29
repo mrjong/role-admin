556e938747daa3826df6a831b935fd7c946a43ec
sun_zhuo <>
2019-11-29 15:04:26 +0800

    fix信用进度查询清空问题

--- src/pages/businessMng/credit_process_mng_page/index.vue
+++ src/pages/businessMng/credit_process_mng_page/index.vue
@@ -25,6 +25,7 @@
                 style="width:100%"
                 format="yyyy-MM-dd"
                 type="datetimerange"
+                :value='formValidate.time'
                 clearable
                 placement="bottom-start"
                 placeholder="请选择操作日期"
